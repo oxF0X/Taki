c34321165afba97fd9f632f5b7912a91477b3a24
eli mor <eliagar10@gmail.com>
2023-04-28 16:56:11 +0300

    edit jsonSerializer

--- Trivia/JsonRequestPacketDeserializer.cpp
+++ Trivia/JsonRequestPacketDeserializer.cpp
@@ -3,17 +3,35 @@
 std::vector<std::uint8_t> JsonRequestPacketDeserializer::serializeResponse(ErrorResponse err)
 {
     nlohmann::json j = err;
-    return nlohmann::json::parse(j);
+    return buildMsg(j,ERROR_RES);
 }
 
 std::vector<std::uint8_t> JsonRequestPacketDeserializer::serializeResponse(LoginResponse l)
 {
     nlohmann::json j = l;
-    return nlohmann::json::parse(j);
+    return buildMsg(j, LOGIN_RES);
 }
 
 std::vector<std::uint8_t> JsonRequestPacketDeserializer::serializeResponse(SignupResponse s)
 {
     nlohmann::json j = s;
-    return nlohmann::json::parse(j);
+    return buildMsg(j, SIGNUP_RES);
+}
+
+std::vector<uint8_t> JsonRequestPacketDeserializer::intToBytes(int value)
+{
+    std::vector<uint8_t> bytes(sizeof(int));
+    std::memcpy(bytes.data(), &value, sizeof(int));
+    return bytes;
+}
+
+std::vector<uint8_t> JsonRequestPacketDeserializer::buildMsg(nlohmann::json j, unsigned int id)
+{
+    std::vector<uint8_t> data = nlohmann::json::parse(std::string(j));
+    std::vector<uint8_t> msg;
+    std::vector<uint8_t> data_size = JsonRequestPacketDeserializer::intToBytes(data.size());
+    msg.push_back(id);
+    std::copy(data_size.begin(), data_size.end(), std::back_inserter(msg));
+    std::copy(data.begin(), data.end(), std::back_inserter(msg));
+    return msg;
 }
--- Trivia/JsonRequestPacketDeserializer.h
+++ Trivia/JsonRequestPacketDeserializer.h
@@ -3,7 +3,13 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <array>
+#include <bitset>
 #include "json.hpp"
+
+#define LOGIN_RES 100
+#define SIGNUP_RES 101
+#define ERROR_RES 102
 
 enum class BYTE : std::uint8_t {};
 
@@ -27,7 +33,12 @@ typedef struct SignupResponse
 
 class JsonRequestPacketDeserializer
 {
+public:
 	static std::vector<std::uint8_t> serializeResponse(ErrorResponse err);
 	static std::vector<std::uint8_t> serializeResponse(LoginResponse l);
 	static std::vector<std::uint8_t> serializeResponse(SignupResponse s);
+
+private:
+	static std::vector<uint8_t> intToBytes(int value);
+	static std::vector<uint8_t> buildMsg(nlohmann::json j, unsigned int id);
 };
