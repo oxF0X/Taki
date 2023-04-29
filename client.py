7da86672cf218abaace23084eca2fc5b1735aaea
F0X <natali.yurchenko123@gmail.com>
2023-04-29 16:08:08 +0300

    Added login

--- client.py
+++ client.py
@@ -1,16 +1,23 @@
 import socket
+import json
+from dataclasses import dataclass
+
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 4444
 
-# Create a TCP/IP socket
+@dataclass
+class Login:
+    "useranme" : str
+    "password" : str
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connecting to remote computer 80
 server_address = (SERVER_IP, SERVER_PORT)
 sock.connect(server_address)
 
-msg = "Hello"
-sock.sendall(msg.encode())
+l = Login("Test", "TheStrongetsPasswordEver")
+jsonData = json.load(l)
+sock.sendall(bin(10) + bin(len(jsonData).zfill(32) + bin(jsonData)).encode())
 
 server_msg = sock.recv(1024)
 print(server_msg.decode())
