f81261eb41d89c384073180d68756abdd15f9851
F0X <natali.yurchenko123@gmail.com>
2023-05-19 20:07:29 +0300

    Finished room class

--- Trivia/Room.h
+++ Trivia/Room.h
@@ -2,14 +2,29 @@
 
 #include <iostream>
 #include <string>
+#include <vector>
+#include "LoggedUser.h"
 
 typedef struct RoomData
 {
 	unsigned int id;
 	unsigned int maxPlayers;
-	unsigned int id;
-	unsigned int id;
-	unsigned int id;
+	unsigned int numOfCards;
+	unsigned int timePerMove;
+	unsigned int isActive;
 	std::string name;
 
 }RoomData;
+
+
+class Room
+{
+public:
+	void addUser(LoggedUser user);
+	void removeUser(LoggedUser user);
+	std::vector<std::string> getAllUsers() const;
+
+private:
+	RoomData m_metatdata;
+	std::vector<LoggedUser> m_users;
+};
