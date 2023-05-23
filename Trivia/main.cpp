f3fc2900c568246deda3d20939538ec62bc70848
F0X <natali.yurchenko123@gmail.com>
2023-05-23 17:14:54 +0300

    Fixed mongo db installation

--- Trivia/main.cpp
+++ Trivia/main.cpp
@@ -1,9 +1,15 @@
 // Trivia.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
+#pragma comment (lib, "C:\\mongo-cxx-driver\\lib\\bsoncxx.lib")
+#pragma comment (lib, "C:\\mongo-cxx-driver\\lib\\mongocxx.lib")
 
+
+#include <mongocxx/client.hpp>
+#include <bsoncxx/builder/stream/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/uri.hpp>
+#include <mongocxx/instance.hpp>
 #include <iostream>
-#pragma comment (lib, "mongocxx.lib")
-#pragma comment (lib, "bsoncxx.lib")
 #include "IRequestHandler.h"
 #include "LoginRequestHandler.h"
 #include "RequestHandlerFactory.h"
