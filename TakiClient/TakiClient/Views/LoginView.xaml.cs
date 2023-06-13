ed625b92a46bcd9993ccdb88f2f0daaf6eb1f8d0
F0X <natali.yurchenko123@gmail.com>
2023-06-13 21:31:23 +0300

    Kinda made menu GUI

--- TakiClient/TakiClient/Views/LoginView.xaml.cs
+++ TakiClient/TakiClient/Views/LoginView.xaml.cs
@@ -8,16 +8,22 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using TakiClient.Modules;
+using System.Runtime.InteropServices;
+
 
 namespace TakiClient
 {
     public partial class LoginView : Window
     {
+
+        //[DllImport("user32.dll")]
+        //public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
 
         public LoginView()
         {
@@ -52,6 +58,16 @@ namespace TakiClient
                 this.DragMove();
             }
         }
+
+/*        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
+        }
+        private void pnlControlBar_MouseEnter(object sender, MouseEventArgs e)
+        {
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }*/
 
     }
 }
--- TakiClient/TakiClient/Views/MenuView.xaml.cs
+++ TakiClient/TakiClient/Views/MenuView.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace TakiClient.Views
 {
@@ -19,9 +21,38 @@ namespace TakiClient.Views
     /// </summary>
     public partial class MenuView : Window
     {
+
+
         public MenuView()
         {
             InitializeComponent();
+            WindowState = WindowState.Maximized;
+
+            WindowStyle = WindowStyle.None;
+            WindowState = WindowState.Maximized;
+
+            double screenWidth = SystemParameters.PrimaryScreenWidth;
+            double screenHeight = SystemParameters.PrimaryScreenHeight;
+
+            Width = screenWidth;
+            Height = screenHeight;
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
         }
     }
 }
