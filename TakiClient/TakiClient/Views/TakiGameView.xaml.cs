36ab4d14e21f353671c7df5830cb5b479882db72
F0X <natali.yurchenko123@gmail.com>
2023-06-23 00:54:49 +0300

    Added basic layout for game window

--- TakiClient/TakiClient/Views/TakiGameView.xaml.cs
+++ TakiClient/TakiClient/Views/TakiGameView.xaml.cs
@@ -39,6 +39,9 @@ namespace TakiClient.Views
             AddImagesToStackPanel(side2, side2Images);
             AddImagesToStackPanel(side3, side3Images);
             AddImagesToStackPanel(side4, side4Images);
+
+            this.SetCenterImage("../Images/back.png");
+
         }
 
         private void AddImagesToStackPanel(StackPanel stackPanel, string[] imagePaths)
@@ -47,9 +50,18 @@ namespace TakiClient.Views
             {
                 Image image = new Image();
                 image.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+                image.Width = 100; // Set the desired width
+                image.Height = 100; // Set the desired height
+                //RotateTransform rotateTransform = new RotateTransform(90);
+                //image.RenderTransform = rotateTransform;
                 stackPanel.Children.Add(image);
             }
         }
-    
-}
+
+        private void SetCenterImage(string imagePath)
+        {
+            centerImage.Source = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+        }
+
+    }
 }
