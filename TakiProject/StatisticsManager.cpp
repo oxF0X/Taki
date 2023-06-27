16a2dbad545ebfe23a96ead0f1b6d0b94d7cbf4a
eli mor <eliagar10@gmail.com>
2023-06-27 17:13:32 +0300

    fix p card

---
+++ TakiProject/StatisticsManager.cpp
@@ -0,0 +1,16 @@
+#include "StatisticsManager.h"
+
+StatisticsManager::StatisticsManager() :m_database(&MongoDB::getDB())
+{
+}
+
+std::vector<std::string> StatisticsManager::getUserStats(std::string username)
+{
+	std::vector<std::string> result;
+	int numOfGames = this->m_database->getNumsOfPlayerGames(username);
+	int numOfWins = this->m_database->getNumOfWins(username);
+	result.push_back(username);
+	result.push_back(std::to_string(numOfGames));
+	result.push_back(std::to_string(numOfWins));
+	return result;
+}
