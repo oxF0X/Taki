b304c7ec770e24d7233d1aca2433be5a0f0dee28
F0X <natali.yurchenko123@gmail.com>
2023-06-29 14:09:19 +0300

    Changes

--- TakiProject/Game.cpp
+++ TakiProject/Game.cpp
@@ -4,10 +4,7 @@ Game::Game(std::vector<std::string> players) : m_currentCard("00"), m_database(&
 {
 	std::srand(static_cast<unsigned>(std::time(nullptr)));
 	std::vector<std::string> cards = { "ST", "ST", "Y1","Y2", "Y3", "Y4", "Y5", "Y6", "Y7", "Y8", "Y9","YD","YP","YS","YT", "B1","B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9","BD","BP","BS","BT", "G1","G3", "G3", "G4", "G5", "G6", "G7", "G8", "G9","GD","GP","GS","GT", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9","RD","RP","RS","RT","Y1","Y2", "Y3", "Y4", "Y5", "Y6", "Y7", "Y8", "Y9","YD","YP","YS","YT", "B1","B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9","BD","BP","BS","BT", "G1","G3", "G3", "G4", "G5", "G6", "G7", "G8", "G9","GD","GP","GS","GT", "R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9","RD","RP","RS","RT","CC","CC" ,"CC" ,"CC" };
-	for (int i = 0; i < 30; i++)
-	{
-		cards.push_back("ST");
-	}
+
 	int size = cards.size();
 	int random_index;
 
